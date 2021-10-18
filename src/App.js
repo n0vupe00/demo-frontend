f27f7b4c83d042d17408712909e67ef6cb1e53ed
Pekka Vuokila <n0vupe00@students.oamk.fi>
2021-10-18 20:01:58 +0300

    Eka

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
+const URL = 'http://localhost/todo/index.php';
 
 function App() {
+
+  const [tasks,setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response)=> {
+      console.log(response.data);
+      setTasks(response.data);
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ol>
+      {tasks?.map(task => (
+        <li key={task.id}>{task.description}</li>
+      ))}
+    </ol>
   );
 }
 
